b457d05a9c07ae7cab13e84f0c4933af0d7f844f
HannesScript <mail@hannessript.dev>
2025-02-07 19:36:22 +0100

    Updated DB to just contain the openings (way faster than big DB)

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import chess
+
 def cdb_to_dict(cdb):
     """
     Convert a CDB object to a dictionary.
@@ -26,3 +28,17 @@ def dict_to_cdb(dict):
     """
     lines = [f"{fen} : {uci}" for fen, uci in dict.items()]
     return "\n".join(lines)
+
+def get_board_fen(board: chess.Board) -> str:
+    fen = board.board_fen() + " " + ("w" if board.turn else "b") + " "
+    if board.has_kingside_castling_rights(chess.WHITE):
+        fen += "K"
+    if board.has_queenside_castling_rights(chess.WHITE):
+        fen += "Q"
+    if board.has_kingside_castling_rights(chess.BLACK):
+        fen += "k"
+    if board.has_queenside_castling_rights(chess.BLACK):
+        fen += "q"
+    if not (board.has_kingside_castling_rights(chess.WHITE) or board.has_queenside_castling_rights(chess.WHITE) or board.has_kingside_castling_rights(chess.BLACK) or board.has_queenside_castling_rights(chess.BLACK)):
+        fen += "-"
+    return fen
