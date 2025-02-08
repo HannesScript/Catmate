586678e3c464a933e4aa17ed5d372a183c6ece05
HannesScript <mail@hannessript.dev>
2025-02-08 08:23:02 +0100

    Updated python wrapper

--- catmate.py
+++ catmate.py
@@ -1,10 +1,8 @@
 import subprocess
 
-executable = ''
-
-def run(fen_string: str, depth: int, database: str) -> str:
+def run(executable_path: str, fen_string: str, depth: int, database: str) -> str:
     try:
-        result = subprocess.run([executable, f"\"{fen_string}\" {depth} \"{database}\""])    # This process will run the executable with the given arguments, which prints out something like "e2e4" to stdout
+        result = subprocess.run([executable_path, f"\"{fen_string}\" {depth} \"{database}\""])    # This process will run the executable with the given arguments, which prints out something like "e2e4" to stdout
         return result.stdout.decode('utf-8').strip()    # This will return the output of the process
     except Exception as e:
         print(f"Error running Catmate: {e}")
